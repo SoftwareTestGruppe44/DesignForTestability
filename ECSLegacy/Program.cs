163a9749e3e8e2ca8b0517ba4da5ce744162888f
Karl Emil Holch Andersen <61682197+KarlHolch@users.noreply.github.com>
2021-02-17 09:53:02 +0100

    Added project to repository

---
+++ ECSLegacy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ECSLegacy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ecs = new ECS(28);
+
+            ecs.Regulate();
+
+            ecs.SetThreshold(20);
+
+            ecs.Regulate();
+        }
+    }
+}
