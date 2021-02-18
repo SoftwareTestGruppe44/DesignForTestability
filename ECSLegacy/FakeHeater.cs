fdca2a7ad52b7e2eac805a5a18941c4abfc25473
Bjørn Larsen <64773431+BjornLarsen1@users.noreply.github.com>
2021-02-18 13:06:11 +0100

    Fakes added

---
+++ ECSLegacy/FakeHeater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECSLegacy
+{
+    class FakeHeater : IHeater
+    {
+        public void TurnOn()
+        {
+            System.Console.WriteLine("Fake Heater is on");
+        }
+
+        public void TurnOff()
+        {
+            System.Console.WriteLine("Fake Heater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
---
+++ ECSLegacy/FakeTempSensor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECSLegacy
+{
+    class FakeTempSensor : ITempSensor
+    {
+        public int Temp { get; set; }
+        public int GetTemp()
+        {
+            return Temp;
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
