67f93a6b8867de4efd46cb4d916fa245520e2797
Karl Emil Holch Andersen <61682197+KarlHolch@users.noreply.github.com>
2021-02-18 16:07:59 +0100

    Added test for window

---
+++ ECSLegacy.Test.Unit/WindowTestClass.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+namespace ECSLegacy.Test.Unit
+{
+    public class WindowTestClass
+    {
+        private IWindow _window;
+        private FakeConsole _console;
+        [SetUp]
+        public void Setup()
+        {
+            _console = new FakeConsole();
+            _window = new Window(_console);
+        }
+
+        [Test]
+        public void Open_OpenWindow_WindowIsOpened()
+        {
+            //Arrange
+            //Act
+            _window.Open();
+            //Assert
+            Assert.AreEqual(_console.ConsoleLine, "Window is open");
+        }
+
+        [Test]
+        public void Close_CloseWindow_WindowIsClosed()
+        {
+            //Arrange
+            //Act
+            _window.Close();
+            //Assert
+            Assert.AreEqual(_console.ConsoleLine, "Window is closed");
+        }
+    }
+}
