17b9b6a4edde7359dffad11a80e186614036ff04
Karl Emil Holch Andersen <61682197+KarlHolch@users.noreply.github.com>
2021-02-18 15:57:56 +0100

    Implemented window

--- ECSLegacy/Window.cs
+++ ECSLegacy/Window.cs
@@ -2,6 +2,26 @@
 {
     public class Window : IWindow
     {
-        
+        private IConsole _console;
+
+        public Window()
+        {
+            _console = new Console();
+        }
+
+        public Window(IConsole console)
+        {
+            _console = console;
+        }
+
+        public void Open()
+        {
+            _console.WriteLine("Window is open");
+        }
+
+        public void Close()
+        {
+            _console.WriteLine("Window is closed");
+        }
     }
 }
