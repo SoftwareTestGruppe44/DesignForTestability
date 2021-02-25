10de55caf14fe8f24fce69f0344fa7ae988a854a
BjornLarsen1 <64773431+BjornLarsen1@users.noreply.github.com>
2021-02-25 13:09:07 +0100

    Added UserValidation class

---
+++ DoorControlSystem/UserValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace DoorControlSystem
+{
+    public class UserValidation
+    { 
+        List<int> idList = new List<int>();
+
+        bool ValidateEntryRequest(int id)
+        {
+            return idList.Contains(id);
+        }
+
+        void AddId(int id)
+        {
+            idList.Add(id);
+        }
+    }
+}
