64e2319b6ad562850bda026040e41e4c0a575fae
Karl Emil Holch Andersen <61682197+KarlHolch@users.noreply.github.com>
2021-02-25 12:59:09 +0100

    Added interface IEntryNotification and implemented class EntryNotification

---
+++ DoorControlSystem/EntryNotification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DoorControlSystem
+{
+    public class EntryNotification : IEntryNotification
+    {
+        public void NotifyEntryGranted(int id)
+        {
+            Console.WriteLine($"Entry granted for ID: {id}");
+        }
+
+        public void NotifyEntryDenied(int id)
+        {
+            Console.WriteLine($"Entry denied for ID: {id}");
+        }
+    }
+}
---
+++ DoorControlSystem/IEntryNotification.cs
@@ -0,0 +1,8 @@
+﻿namespace DoorControlSystem
+{
+    public interface IEntryNotification
+    {
+        void NotifyEntryGranted(int id);
+        void NotifyEntryDenied(int id);
+    }
+}
