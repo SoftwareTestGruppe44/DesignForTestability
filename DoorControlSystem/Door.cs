3898e979bc247a9928782fbd4efd007cc152af74
au636574 <mikkel.thomasen@gmail.com>
2021-02-25 12:59:41 +0100

    Implementation of IDoor and Door plus methods.

---
+++ DoorControlSystem/Door.cs
@@ -0,0 +1,18 @@
+﻿namespace DoorControlSystem
+{
+    public class Door : IDoor
+    {
+
+        public bool DoorIsOpen { get; private set; } = false;
+
+        public void Open()
+        {
+            DoorIsOpen = true;
+        }
+
+        public void Close()
+        {
+            DoorIsOpen = false;
+        }
+    }
+}
